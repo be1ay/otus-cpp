89b6cb107feb5cf12a09af42294b953422a90dcb
be1ay <be1ay@users.noreply.github.com>
2025-10-16 15:35:19 +0000

    deploy: 5c4e7a09fe541392457386af5e44e3642fe3f9ab

--- search/functions_1.js
+++ search/functions_1.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['circle_0',['Circle',['../classmyeditor_1_1Circle.html#ae9de0b5e2432202f87ddc1028fd6393a',1,'myeditor::Circle']]],
   ['clearshapes_1',['clearShapes',['../classmyeditor_1_1Controller.html#aa64bbd8f96fb9a86a7219655d6fa5173',1,'myeditor::Controller']]],
-  ['controller_2',['Controller',['../classmyeditor_1_1Controller.html#a169ede0ab28e0c1b52463bb775a4c200',1,'myeditor::Controller']]],
+  ['controller_2',['Controller',['../classmyeditor_1_1Controller.html#a7709f1482768e1152362ef4db42e0d02',1,'myeditor::Controller']]],
   ['createshape_3',['createShape',['../classmyeditor_1_1Controller.html#a62509433a3485485c795e05c4dfff253',1,'myeditor::Controller']]]
 ];
